e208f1686fd04bbff58dee5035c434dda3574da9
Ruslan Gusseinov <r.gusseinov@mail.ru>
2023-11-20 00:35:38 +0600

    WIP. Add twig

--- index.php
+++ index.php
@@ -3,13 +3,18 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Psr7\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require __DIR__ . '/vendor/autoload.php';
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-	$response->getBody()->write("Hello world!");
+$app->get('/', function (Request $request, Response $response, $args) use ($view) {
+	$body = $view->render('index.twig');
+	$response->getBody()->write($body);
 	return $response;
 });
 
